46fde1b3bce76405a5e9a1c98b0656c90f52d5ca
Olya <120562039+OlyaSpaka@users.noreply.github.com>
2025-01-13 19:51:10 +0200

    Add files via upload

---
+++ user-service/src/test/java/com/craft_organizer/user_service/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.craft_organizer.user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
